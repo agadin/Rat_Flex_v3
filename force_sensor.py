676008d014b4bd9f54b8869f205569aff26f10cd
agadin <a.gadin@wustl.edu>
2024-11-14 14:21:42 -0600

    changed default usb

--- force_sensor.py
+++ force_sensor.py
@@ -2,7 +2,7 @@
 import serial
 
 class ForceSensor:
-    def __init__(self, port='/dev/ttyUSB0', baudrate=9600, timeout=1):
+    def __init__(self, port='/dev/ttyUSB1', baudrate=9600, timeout=1):
         self.port = port
         self.baudrate = baudrate
         self.timeout = timeout
