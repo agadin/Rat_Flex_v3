385dc49d46a02f30b7b27f4697e97e43f45a3981
Alexander Gadin <112425293+agadin@users.noreply.github.com>
2024-11-14 14:02:21 -0600

    Update simple_main.py

--- simple_main.py
+++ simple_main.py
@@ -8,19 +8,35 @@ import redis
 redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 
+def send_protocol_path(protocol_path):
+    server_address = ('localhost', 8765)  # Server's address and por
+    try:
+        # Create a TCP/IP socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            # Connect the client to the server
+            client_socket.connect(server_address)
+
+            # Send the protocol path to the server
+            client_socket.sendall(protocol_path.encode('utf-8'))
+
+            # Optionally, you can wait for a response from the server
+            # response = client_socket.recv(1024).decode('utf-8')
+            # print(f"Server response: {response}")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 def run_protocol(protocol_path):
-    #redis_client.set('protocol_trigger', protocol_path)
-    python_path= 'base/bin/python'
-    result = subprocess.run([python_path, 'protocol_runner.py', protocol_path], capture_output=True, text=True)
+    redis_client.set('protocol_trigger', protocol_path)
+    print(f"Triggered protocol: {protocol_path}")
 
-    if result.returncode == 0:
-        print("Script ran successfully")
-        print(result.stdout)  # If you want to capture and print output from the script
-    else:
-        print("Error running script")
-        print(result.stderr)  # If you want to capture and print error output
 
+
+if __name__ == "__main__":
+    # Provide the protocol path to trigger_script()
+    #protocol_path = "path_to_your_protocol_file.txt"
+    #trigger_script(protocol_path)
+    print('Hello')
 
 
 # List protocol files in the protocols folder
