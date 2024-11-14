2312ab0692c47ce6932b01aedc92f30a88e4c867
agadin <a.gadin@wustl.edu>
2024-11-14 11:29:52 -0600

    patched asyncio problems

--- simple_main.py
+++ simple_main.py
@@ -12,7 +12,7 @@ def run_protocol(protocol_path):
     asyncio.run(send_command(protocol_path))
 
 # List protocol files in the protocols folder
-protocol_folder = 'protocols'
+protocol_folder = './protocols'
 protocol_files = [f for f in os.listdir(protocol_folder) if os.path.isfile(os.path.join(protocol_folder, f))]
 
 st.title("Stepper Motor Control")
