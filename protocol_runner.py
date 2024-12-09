2a6a81c7a9d8151b0213ce19518a49c399de8c51
agadin <a.gadin@wustl.edu>
2024-12-09 00:34:44 -0600

    calibrate each force v1 (does not render it). Also updated move till force

--- protocol_runner.py
+++ protocol_runner.py
@@ -134,6 +134,7 @@ def start_server():
             protocol_path = redis_client.get("protocol_trigger")
             print(f"Checking for protocol trigger: {protocol_path}")
             shared_memory_error=redis_client.get("shared_memory_error")
+            print(f"Checking for shared memory error: {shared_memory_error}")
             if shared_memory_error ==1:
                 print("Shared memory error detected. Recreating shared memory.")
                 redis_client.set("shared_memory_error", 0)
