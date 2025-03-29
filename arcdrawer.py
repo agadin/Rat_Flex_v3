16a5d639b2742339ad2fc0bfa31f8406e6cb4ceb
agadin <a.gadin@wustl.edu>
2025-03-29 13:32:58 -0500

    fix angle calculation in arcdrawer.py for Move_to_angle_jog command

--- arcdrawer.py
+++ arcdrawer.py
@@ -71,15 +71,16 @@ class AdvancedCurvedSlider(tk.Canvas):
         return value (math.pi / 180)
 
     def update_blue_position(self):
-                """
-                Update the blue circle's (handle) position on the canvas based on its current angle.
-                """
-                x = self.center_x - self.radius * math.cos(self.blue_angle)
-                y = self.center_y + self.radius * math.sin(self.blue_angle)
-                self.coords(self.blue_circle,
-                            x - self.handle_radius, y - self.handle_radius,
-                            x + self.handle_radius, y + self.handle_radius)
-                self.angle_var.set(str(self.value_from_angle(self.blue_angle)))
+        """
+        Update the blue circle's (handle) position on the canvas based on its current angle.
+        """
+        blue_angle =180- self.blue_angle
+        x = self.center_x + self.radius * math.cos(blue_angle)
+        y = self.center_y - self.radius * math.sin(blue_angle)
+        self.coords(self.blue_circle,
+                    x - self.handle_radius, y - self.handle_radius,
+                    x + self.handle_radius, y + self.handle_radius)
+        self.angle_var.set(str(self.value_from_angle(self.blue_angle)))
 
     def set_blue_angle(self, angle_degrees):
         """
